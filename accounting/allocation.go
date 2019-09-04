ba75e1fd4c366fb62ee0abe0ea8acef52e420324
Mac Ma <mac@ninjadojo.com.au>
2019-09-04 15:13:49 +0800

    fix bug with GET overpayment allocation amount

--- accounting/allocation.go
+++ accounting/allocation.go
@@ -3,7 +3,14 @@ package accounting
 //Allocation allocated an overpayment or Prepayment to an Invoice
 type Allocation struct {
 
-	// the amount being applied to the invoice
+	// note: the Amount for GET and PUT uses similar but differnt naming
+
+	// the amount being applied to the invoice (GET)
+	// https://developer.xero.com/documentation/api/overpayments#GET
+	Amount float64 `json:"Amount,omitempty" xml:"Amount,omitempty"`
+
+	// the amount being applied to the invoice (PUT)
+	// https://developer.xero.com/documentation/api/overpayments#PUT
 	AppliedAmount float64 `json:"AppliedAmount,omitempty" xml:"AppliedAmount,omitempty"`
 
 	// the date the prepayment is applied YYYY-MM-DD (read-only). This will be the latter of the invoice date and the prepayment date.
